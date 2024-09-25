c60254a7bdcd1b51857bc2e4312d2c1d1137a3be
XTL676 <tianluoqaq@qq.com>
2024-09-25 16:42:12 +0800

    初始化仓库

---
+++ src/main/kotlin/org/xlyo/anywild/AnywildDataGenerator.kt
@@ -0,0 +1,9 @@
+package org.xlyo.anywild
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object AnywildDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
