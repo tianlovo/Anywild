f80a512fc967b3f6a8ef6bd98b1a767a1e316ef2
tianlovo <142667805+tianlovo@users.noreply.github.com>
2024-09-25 16:39:32 +0800

    Delete Fabric/anywild-1.19.2 directory

--- Fabric/anywild-1.19.2/src/main/kotlin/org/xlyo/anywild/AnywildDataGenerator.kt
+++
@@ -1,9 +0,0 @@
-package org.xlyo.anywild
-
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-
-object AnywildDataGenerator : DataGeneratorEntrypoint {
-	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-	}
-}
